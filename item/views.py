e1af62e0b08b981ec0c88510e62bacb9586d5157
FacundoBe <facundobellomo@gmail.com>
2023-03-10 17:21:10 -0300

    comenzada l apagina d ecarga de items

--- item/views.py
+++ item/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required 
 from .models import Item
+from .forms import NewItemForm
+
 
 def detail(request,pk):
     item= get_object_or_404(Item, pk=pk)
     related_items=Item.objects.filter(category=item.category, is_sold=False).exclude(pk=pk)[0:3]
     #el exclude es para que no incluya el item actual, ademas guardo solo los primeros 3 items
     return render(request, 'detail.html', {'item':item,'related_items':related_items})
+
+@login_required
+def new(request):
+    form=NewItemForm()
+    render (request,'new.html',{'form':form,'title':'New Item'})
--- main/views.py
+++ main/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from item.models import  Category,Item
 from .forms import SignupForm
 
@@ -16,11 +16,16 @@ def contact(request):
     return render(request, 'contact.html',{})
 
 def signup(request):
-    form = SignupForm()
+    
     if request.method=='POST':
         form = SignupForm(request.POST)
-        
+        if form.is_valid():
+            form.save()
+
+            return redirect('login')
+    else: # We are not i post, so empty form is rendered   
+        form = SignupForm()
+
     return render(request,'signup.html',{'form':form})
     
 
-    
